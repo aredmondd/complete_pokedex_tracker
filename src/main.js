55819419ab93370a8b93ac42d47411efe4ca18aa
aiden redmond <adnrdmnd@gmail.com>
2026-07-08 19:28:32 -0400

    load images better

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import "./app.css";
 import App from "./App.svelte";
 import { mount } from "svelte";
+import pokedex from "./data/pokemon.json";
+import { SPRITE_DIRECTORY } from "./lib/utils/constants.js";
 
 const app = mount(App, {
   target: document.getElementById("app")
 });
+
+const preloadSprites = () => {
+  for (const pokemon of pokedex.pokemon) {
+    new Image().src = `${SPRITE_DIRECTORY}${pokemon.id}.png`;
+  }
+};
+
+if ("requestIdleCallback" in window) {
+  requestIdleCallback(preloadSprites, { timeout: 3000 });
+} else {
+  setTimeout(preloadSprites, 2000);
+}
 
 export default app;
